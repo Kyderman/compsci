a2abeff07c906f4105d4f295584ea6775102bd9c
Kyle Selman <kyle@kyleselman.co.uk>
2014-02-28 04:28:33 +0000

    use machine IP too? or not???

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,17 +4,9 @@ module ApplicationHelper
     return request.remote_ip
   end
 
-  require 'socket'
  
   def local_ip
-  orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
- 
-  UDPSocket.open do |s|
-    s.connect '64.233.187.99', 1
-    s.addr.last
-  end
-  ensure
-  Socket.do_not_reverse_lookup = orig
+  return request.env['REMOTE_ADDR']
   end
   
   def ip
